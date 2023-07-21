202ea6b0cd0204f0f849e473e2c2f4455f9c8b0e
Bhupendra <bhupendranasare@gmail.com>
2023-07-21 22:38:41 +0530

    Ready for Depolyment

--- src/components/Products.js
+++ src/components/Products.js
@@ -39,7 +39,6 @@ const Products = () => {
    
    
     try {
-       
       const response = await axios.get(
         "https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json"
       );
